f73b4764179cf4ae74e4b16e166587af0a92d1a6
Xolani Victory <psymbiotic@Xolanis-MacBook-Pro.local>
2024-01-04 18:05:18 -0800

    Figuring out the owner functionality

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 8080;
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+    res.sendFile(__dirname + '/index.html');
 });
 
 // hello world route
@@ -21,31 +21,31 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.send(pets);
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+const owner = req.query.owner;
 
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
+    const pet = pets.filter(pet => pet.owner === owner);
 
     // send the pet as a response
-
+res.send(`${owner} is the owner of ${pet}`)
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
 
-
+    const petName = req.params.name
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.name === name);
+    const pet = pets.find(pet => pet.name === petName);
 
     // send the pet as a response
-
+    res.send(`${petName} is the name of the ${pet.breed}.`)
 });
 
 app.listen(PORT, () => {
